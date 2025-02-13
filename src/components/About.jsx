63eadd2e45a83e10afc7179d44ca9d0c3acab3f5
AlbertNewton254 <miguelmochizukisilva@gmail.com>
2025-02-13 20:07:19 -0300

    feat: adds profile picture with circular shape

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <p>Sou entusiasta das ciências exatas: estudei sozinho todo o conteúdo de matemática, de física e de química do Ensino Médio durante a pandemia, e aprimorei posteriormente meus conhecimento em matemática, sob certificação do MIT em Cálculo Diferencial e Integral (2021-2022). </p>
             <p>Fui pesquisador PIBIC/CNPq júnior em Matemática pela UFPB, trabalhando no projeto “Equações Diferenciais Parciais e Análise Geométrica” (2022-2023), sob a orientação do professor Dr. João Marcos Bezerra do Ó. Nele, estudei a matemática que é usada como base da física e da química modernas, e fui agraciado com o prêmio Jovem Pesquisador 2023, com o trabalho “Análise e Geometria em Variedades Riemannianas”.</p>
             <p>Tenho experiência com programação, utilizando Python para projetos pessoais, além de ter um domínio avançado de inglês, intermediário de espanhol e francês, o que me permitiu trabalhar diretamente com textos de pesquisa em diferentes idiomas.</p>
-            <p>Além disso, obtive mais de 800 pontos em Matemática no Enem 2024, com 40 questões acertadas. Minha missão é ajudar você a aprender de forma eficiente e acessível as ciências exatas, tornando-as uma vocação e um direito a ser exercido por todos!</p>
+            <p>Além disso, obtive mais de 800 pontos em Matemática no Enem 2024, com 40 questões acertadas. Em suma, minha missão é ajudar você a aprender de forma eficiente e acessível as ciências exatas, tornando-as uma vocação e um direito a ser exercido por todos!</p>
         </section>
     );
 };
